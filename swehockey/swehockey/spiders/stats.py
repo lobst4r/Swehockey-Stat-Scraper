cf0073fb13f162defe06bf1b7dbb90d2f359e10a
Robin W <thelobst4r@gmail.com>
2022-03-09 03:22:41 +0100

    Fix errors in basic stats parser

--- swehockey/swehockey/spiders/stats.py
+++ swehockey/swehockey/spiders/stats.py
@@ -40,14 +40,21 @@ class StatsSpider(scrapy.Spider):
         pp_time_team_1 = self.get_stats_by_period(game_info, 8, 3, False)
         pp_time_team_2 = self.get_stats_by_period(game_info, 8, 6, False)
         pp_perc_team_1 = game_info.xpath("//tr[8]/td[2]/strong/text()").get().strip()
-        pp_perc_team_1 = float(pp_perc_team_1.replace(",", ".")
+        try:
+            pp_perc_team_1 = float(pp_perc_team_1.replace(",", ".")
                                .replace("%", ""))
+        except:
+            pp_perc_team_1 = None
         pp_perc_team_2 = game_info.xpath("//tr[8]/td[5]/strong/text()").get().strip()
-        pp_perc_team_2 = float(pp_perc_team_2.replace(",", ".")
+        try:
+            pp_perc_team_2 = float(pp_perc_team_2.replace(",", ".")
                                .replace("%", ""))
+        except:
+            pp_perc_team_2 = None
 
-        spectators = game_info.xpath("//td[@class='tdInfoArea']/div[4]/text()").get().strip()
-        spectators = int(spectators.split(":")[1].strip())
+        spectators = clean(game_info.xpath("//td[@class='tdInfoArea']/div[4]/text()").get())
+        if spectators:
+            spectators = int(clean(spectators.split(":")[1]))
         
         # Get score by period
         score = game_info.xpath("//td[@class='tdInfoArea']/div[2]/text()").get().strip()
@@ -59,6 +66,7 @@ class StatsSpider(scrapy.Spider):
         score_team_2 = [goals[1] for goals in score]
 
         yield {
+            'swehockey_id': swehockey_id,
             'game_stats': {
                 'team_1': teams[0],
                 'team_2': teams[1],
@@ -67,10 +75,16 @@ class StatsSpider(scrapy.Spider):
                 'arena': arena,
                 'shots_team_1': shots_team_1,
                 'shots_team_2': shots_team_2,
+                'shots_sum_team_1': sum(shots_team_1),
+                'shots_sum_team_2': sum(shots_team_2),
                 'saves_team_1': saves_team_1,
                 'saves_team_2': saves_team_2,
+                'saves_sum_team_1': sum(saves_team_1),
+                'saves_sum_team_2': sum(saves_team_2),
                 'pim_team_1': pim_team_1,
                 'pim_team_2': pim_team_2,
+                'pim_sum_team_1': sum(pim_team_1),
+                'pim_sum_team_2': sum(pim_team_2),
                 'pp_time_team_1': pp_time_team_1,
                 'pp_time_team_2': pp_time_team_2,
                 'pp_perc_team_1': pp_perc_team_1,
@@ -78,8 +92,7 @@ class StatsSpider(scrapy.Spider):
                 'score': score,
                 'score_team_1': score_team_1,
                 'score_team_2': score_team_2,
-                'spectators': spectators,
-                'swehockey_id': swehockey_id
+                'spectators': spectators
              }
         }
 
